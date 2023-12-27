f382b367474b60f67044aead09d7fa2159d3e9fe
biplove subedi <subedi.biplove10@gmail.com>
2023-12-27 19:09:36 +0545

    merging

--- src/pages/home/home.page.jsx
+++ src/pages/home/home.page.jsx
@@ -2,14 +2,19 @@ import { ButtonComponent } from "../../components/common/button/button.component
 import { Form } from "react-bootstrap";
 import { H1 } from "../../components/common/heading/heading.component";
 import { Heading } from "../../components/common/heading/heading.component";
-const HomePage = () => {
+// import { useState } from "react";
+const HomePage = ({ title }) => {
+  // const [title,titleset] = useState("Default value")
+
   const clickListen = () => {
+    // titleset("updated title")
     console.log("i have been clicked");
   };
+
   return (
     <div>
-      <H1 value={"title"}></H1>
-      <Heading type="h1" value={"title"}></Heading>
+      <H1 value={title}></H1>
+      <Heading type="h1" value={title}></Heading>
       <ButtonComponent
         onClick={clickListen}
         type={"button"}
